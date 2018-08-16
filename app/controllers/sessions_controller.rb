c30f8995cd75274c593beeac58587b7fc631bb8c
Neil Seejoor <nvseejoor@gmail.com>
2018-08-16 19:55:06 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
       session[:username] = params[:username]
       redirect_to '/'
     else
-      redirect_to :new
+      redirect_to '/login'
 
   end
 
